fe1a268bbc7a53278a174c3fed5c5b9b75ba4463
Neechelle James <neechelle.james@gmail.com>
2023-03-27 17:33:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = (["Charlie", "Samip", "Ali"], "birthday");
+const event = "surprise"
+   function writeCards(name, event) {
+    let message = [];
+        for (let i = 0; i < name.length; i++) {
+            message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+            }
+        return message;
+    }
+    console.log(writeCards(name, event))
+
+
+    function countDown() {
+        let countDown = 0;
+        while (countDown < 11) {
+            console.log(countDown++)
+        }
+    }
